8f65582b065f77306f9845bcac716f1095c6e0e0
Ariel Batoon <info.arielbatoon@gmail.com>
2023-09-06 15:01:22 +0800

    Updated tbl_user migration db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->integer('is_verified');
+            $table->string('profile_img')->nullable();
             $table->integer('credits_amount');
             $table->integer('flag');
             $table->timestamps();
