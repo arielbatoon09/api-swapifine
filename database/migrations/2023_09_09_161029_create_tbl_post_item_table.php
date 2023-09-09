5bcc8d49f4d7c46e76ac9b2b39ba521ca426afe6
Ariel Batoon <info.arielbatoon@gmail.com>
2023-09-10 00:24:05 +0800

    New Data

--- database/migrations/2023_09_09_161029_create_tbl_post_item_table.php
+++ database/migrations/2023_09_09_161029_create_tbl_post_item_table.php
@@ -11,9 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tbl_category', function(Blueprint $table) {
+        Schema::create('tbl_post_item', function (Blueprint $table) {
             $table->id();
-            $table->string('category_name');
             $table->timestamps();
         });
     }
@@ -23,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tbl_post_item');
     }
 };
