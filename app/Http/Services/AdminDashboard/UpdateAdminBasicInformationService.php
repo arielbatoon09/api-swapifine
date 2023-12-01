cd0c061e059a3031544c03b6eba80b41d8d108ae
Ariel Batoon <info.arielbatoon@gmail.com>
2023-12-02 01:27:28 +0800

    Added Update Basic information

--- app/Http/Services/AdminDashboard/UpdateAdminBasicInformationService.php
+++ app/Http/Services/AdminDashboard/UpdateAdminBasicInformationService.php
@@ -12,7 +12,23 @@ class UpdateAdminBasicInformationService
     public static function UpdateAdminBasicInformation(Request $request)
     {
         try {
-            return 'asdasdas';
+            $admin = Admin::findOrFail(Auth::user()->id);
+
+            if (!$admin || !$request->fullname) {
+                return response([
+                    'status' => 'error',
+                    'data' => 'Not found record!',
+                ]);
+            }
+
+            $admin->update([
+                'fullname' => $request->fullname,
+            ]);
+
+            return response([
+                'status' => 'success',
+                'data' => 'Updated successfully basic information.',
+            ]);
         } catch (Throwable $e) {
             return 'Error Catch: ' . $e->getMessage();
         }
